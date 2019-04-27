0ed8a4987fcb9853e24348b296f0c665684c722e
mkrajnak <mkrajnak>
2019-04-27 19:56:42 +0200

    Update

--- solution.py
+++ solution.py
@@ -77,13 +77,14 @@ def parse_input(input):
 
 if __name__ == '__main__':
     PUBKEY = parse_input(sys.argv[1])
-    curve = Curve (
+    curve = Curve(
         fp=0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff,
         a=-0x3,
         b=0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b,
         P=(0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296,
            0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5)
         )
+    # test = Curve(fp=23, a=1, b=1, P=(13, 16))
     try:
         curve.is_valid_point(PUBKEY)
     except Exception:
